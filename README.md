27c14c6640eca784a571564abd4de18194c9d298
Gareth Wright <garpunkal@gmail.com>
2022-03-10 12:20:02 +0000

    Init

---
+++ README.md
@@ -0,0 +1,15 @@
+# punkDropDownList
+
+An app_plugin for Umbraco that provides an Umbraco Drop Down List with Text and Key.
+
+There is an included PropertyValueConverter to provide you with the value of the selected item in the CMS. 
+
+# Nuget
+
+`Install-Package punkDropDownList`
+
+https://www.nuget.org/packages/punkDropDownList/
+
+# Compatibility
+
+- Umbraco 9+ through nuget package but files should work on older version of Umbraco. 
---
+++ punkDropDownList/App_Plugins/punkDropDownList/ValueConverters/PunkDropDownListValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PropertyEditors;
+
+
+namespace punk.DropDownList.ValueConverters
+{
+    public class PunkDropDownListPropertyValueConverter : PropertyValueConverterBase
+    {
+        public override bool IsConverter(IPublishedPropertyType propertyType)
+            => propertyType.EditorAlias == "punkDropDownList";
+
+        public override Type GetPropertyValueType(IPublishedPropertyType propertyType)
+            => typeof(string);
+
+        public override bool? IsValue(object value, PropertyValueLevel level)
+            => value?.ToString() != "[]";
+
+        public override object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview)
+            => source?.ToString();
+
+        public override object ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
+            => inter ?? null;
+    }
+}
